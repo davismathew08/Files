3a543077f96810b844fb5cd0adab44c42d15a135
davismathew08 <57518339+davismathew08@users.noreply.github.com>
2020-08-25 18:08:09 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/formapplication/HomeActivity.java
@@ -0,0 +1,17 @@
+package com.example.formapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+}
---
+++ app/src/main/java/com/example/formapplication/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.formapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button login;
+    private TextView forgotpassword;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        login=findViewById(R.id.loginbutton);
+        forgotpassword=findViewById(R.id.forgotpassword);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+        forgotpassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
